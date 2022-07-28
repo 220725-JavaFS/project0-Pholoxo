81c9ec61c4c6db6a39314e7564f4d3e258d14f34
Max Morales <maxyMorales52@gmail.com>
2022-07-28 17:36:16 -0400

    Made changes
    description

---
+++ src/main/java/com/revature/BankInfo.java
@@ -0,0 +1,14 @@
+package com.revature;
+import java.util.Scanner;
+
+public class BankInfo {
+	private String name;
+	private String Account;
+	private String Type;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void open() {
+		System.out.println("");
+	}
+}
---
+++ src/main/java/com/revature/BankLogin.java
@@ -0,0 +1,32 @@
+package com.revature;
+import java.util.Scanner;
+
+public class BankLogin {
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//starts the app
+		System.out.print("Enter Username: ");
+		
+		String username = sc.next().trim();
+		
+		System.out.print("Enter Password: ");
+		
+		String password = sc.next().trim();
+		
+		String su = "John";
+		
+		String sp = "boobs";
+		
+		
+		if(!su.equals(username) || !sp.equals(password)) {
+			System.out.println("Incorrect userame or password");
+		}
+		if(su.equals(username) && sp.equals(password)) {
+			System.out.println("correct");
+		}
+		
+	}
+}
